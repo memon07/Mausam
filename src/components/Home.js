a37449be9f6a13544199c6f927bcc02cf3f8820b
Shoyeb Memon <shoyebmemon@Shoyebs-MacBook-Air.local>
2020-03-19 22:40:52 +0530

    weather search bar working

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,40 +1,49 @@
-import React , { useEffect } from 'react'
+import React , { useState ,useEffect } from 'react'
 import { connect } from 'react-redux'
 import {Button } from 'antd'
 
+import Weather from './Weather'
+
 import history from '../history'
+import '../css/Home.css'
 
 function Home(props) {
 
-    useEffect(() => {
-        // console.log(user.user)
-        console.log(props.user ?props.user.payload : '')
-        // setUser(user.user)
-    }, [])
+    const [value, setValue] = useState('');
 
-    if(props.user === null || props.user === 'undefined'){
-        // console.log(user.payload)
-        return <>
-            <div className="container">
-                <div className="row">
-                    <div className="col-12 pt-4 text-center">
-                        <h3 className="text-center">Please login to access our module</h3>
-                        <Button onClick={() => {history.push("/login")}}>
-                            Login in</Button>
-                    </div>
-                </div>
-            </div>
-        </>
-    }
+    // if(props.user === null || props.user === 'undefined'){
+    //     return <>
+    //         <div className="container">
+    //             <div className="row">
+    //                 <div className="col-12 pt-4 text-center">
+    //                     <h3 className="text-center">Please login to access our module</h3>
+    //                     <Button onClick={() => {history.push("/login")}}>
+    //                         Login in</Button>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     </>
+    // }
     return (
         <div className="container">
             <div className="row">
                 <div className="col-12">
-                    <h3>Welcome ,</h3>
-                    <h5>
-                        {props.user?props.user.payload.email : ''}
+                    <h3 className="mb-1 mt-3">Welcome ,</h3>
+                    <h5 className="home-username">
+                        {props.user?props.user.payload.email.split('@')[0] : ''}
                     </h5>
                 </div>
+
+                <div className="col-12">
+                    <div className="search-text pb-2">
+                        <h5>
+                            Search your favourite city's weather 
+                        </h5>
+                    </div>
+                    <Weather/>
+                </div>
+
+
             </div>
         </div>
     )
---
+++ src/components/Weather.js
@@ -0,0 +1,80 @@
+import React , { Component } from 'react'
+import ReactAutocomplete from 'react-autocomplete'
+import { Button } from 'antd'
+import axios from 'axios'
+
+import cities from './city.json'
+import '../css/Weather.css'
+
+class Weather extends Component {
+
+    constructor (props) {
+      super(props)
+      this.state = {
+        value: '',
+        weather: ''
+      }
+    }
+
+    searchWeather = () => {
+        console.log(this.state.value)
+        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=589eede1cda9e557e2aa4273d1b5b36f`, { 
+        }).then((response) => {
+            console.log(response.data)
+            this.setState(
+              {weather : response.data}
+              )
+              // this.setState({
+              //   weather: [...this.state.weather, response.data]
+              // })
+
+              console.log('...',this.state.weather)
+        }).catch((error)=> {
+            console.log(error)
+        })
+    }
+  
+    render() {
+      return (
+          <>
+          <div className="row">
+            <div className="col-12 pb-5">
+              <ReactAutocomplete
+              className="ant-input"
+                  items={[
+                      ...cities
+                  ]}
+                  shouldItemRender={(item, value) => item.city.toLowerCase().indexOf(value.toLowerCase()) > -1}
+                  getItemValue={item => item.city}
+                  renderItem={(item, highlighted) =>
+                  <div
+                      key={item.id}
+                      style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}}
+                  >
+                      {item.city}
+                  </div>
+                  }
+                  value={this.state.value}
+                  onChange={e => this.setState({ value: e.target.value })}
+                  onSelect={value => this.setState({ value })}
+              />
+              <Button onClick={this.searchWeather} size="large" className="ml-2" >Search</Button>
+            </div>
+            {this.state.weather ?
+            <div className="col-12">
+              <span className="temprature-div">
+                {Math.trunc(this.state.weather.main.temp - 273.15)} ° C
+              </span>
+              <img src={`https://openweathermap.org/img/w/${this.state.weather.weather[0].icon}.png`} alt="logo"/>
+              <h4>
+                {this.state.weather ?this.state.weather.weather[0].description : ''}
+              </h4>
+            </div>
+            :''}
+          </div>
+          </>
+      )
+    }
+  }
+
+  export default Weather
